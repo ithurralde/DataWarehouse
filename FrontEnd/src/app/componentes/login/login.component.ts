6fbdb5451876afdb788bb42ba002e6461f019d59
ithurralde <santi.ithurralde@gmail.com>
2021-07-26 17:54:41 -0300

    arreglos en el login con eltema del header

--- FrontEnd/src/app/componentes/login/login.component.ts
+++ FrontEnd/src/app/componentes/login/login.component.ts
@@ -22,21 +22,22 @@ export class LoginComponent implements OnInit {
     console.log(this.usuario);
     console.log(this.contrasenia);
     console.log(this.log);
-    if (this.log)
-      this.log.style.display = "block";
+    // if (this.log)
+    //   this.log.style.display = "block";
     // let user = new Usuario(this.usuario, "Wayne", "brenwayne13@gmail.com", "Argentina", "Buenos Aires");
     // this.dbService.crearUsuario(user);
     let existe = this.usuarioService.existeUsuario(this.usuario, this.contrasenia);
     if (existe == true){
+      if (this.log)
+      this.log.style.display = "block";
       this.usuarioService.logear(this.usuario, this.contrasenia)
       .subscribe(
         response => 
             {console.log("Exito al loguear: " + response);
             console.log(response);
-            this.router.navigate(["usuarios"]);
+            this.router.navigate(["contactos"]);
             this.usuarioService.isAdmin(this.usuario).subscribe(
               (admin:any) => {
-                console.log("holaaaaaaaaaaaaaaaaaaaaa");
                 console.log(admin);
                 if (this.admin && admin[0].admin === 1)
                   this.admin.style.display = "block";
